4637903abcd51e456b96a955e7cad48a0734437c
Joern Rennecke <joern.rennecke@embecosm.com>
2022-03-05 03:40:15 +0000

    Finish initial implementation of ext-dce pass.

--- gcc/ext-dce.cc
+++ gcc/ext-dce.cc
@@ -23,8 +23,11 @@ along with GCC; see the file COPYING3.  If not see
 #include "backend.h"
 #include "rtl.h"
 #include "memmodel.h"
+#include "insn-config.h"
 #include "emit-rtl.h"
+#include "recog.h"
 #include "cfganal.h"
+#include "tree-pass.h"
 #include "rtl-iter.h"
 
 /* We consider four bit groups for liveness:
@@ -33,12 +36,195 @@ along with GCC; see the file COPYING3.  If not see
    bit 16..31
    bit 32..BITS_PER_WORD-1  */
 
+bitmap
+ext_dce_process_bb (basic_block bb, bitmap livenow, bool modify)
+{
+  rtx_insn *insn;
+
+  FOR_BB_INSNS_REVERSE (bb, insn)
+    {
+      subrtx_iterator::array_type array;
+
+      if (!INSN_P (insn))
+	continue;
+
+      bitmap live_tmp = BITMAP_ALLOC (NULL);
+
+      /* First, process the sets.  */
+      FOR_EACH_SUBRTX (iter, array, PATTERN (insn), NONCONST)
+	{
+	  const_rtx x = *iter;
+	  if (GET_CODE (x) == SET || GET_CODE (x) == CLOBBER)
+	    {
+	      unsigned bit = 0;
+	      unsigned HOST_WIDE_INT mask = GET_MODE_MASK (GET_MODE (x));
+	      x = SET_DEST (x);
+	      if (GET_CODE (x) == SUBREG)
+		{
+		  bit = SUBREG_BYTE (x).to_constant () * BITS_PER_UNIT;
+		  if (WORDS_BIG_ENDIAN)
+		    bit = (GET_MODE_BITSIZE (GET_MODE (x)).to_constant ()
+			   - BITS_PER_WORD - bit);
+		  mask = GET_MODE_MASK (GET_MODE (SUBREG_REG (x))) << bit;
+		  if (!mask)
+		    mask = -100000000ULL;
+		  x = SUBREG_REG (x);
+		}
+	      else if (GET_CODE (x) == STRICT_LOW_PART)
+		{
+		  x = XEXP (x, 0);
+		}
+	      if (REG_P (x))
+		{
+		  HOST_WIDE_INT rn = REGNO (x);
+		  for (HOST_WIDE_INT i = 4 * rn; i < 4 * rn + 4; i++)
+		    if (bitmap_bit_p (livenow, i))
+		      bitmap_set_bit (live_tmp, i);
+		  int start = (bit == 0 ? 0 : bit == 8 ? 1
+			       : bit == 16 ? 2 : 3);
+		  int end = ((mask & ~0xffffffffULL) ? 4
+			     : (mask & 0xffff0000) ? 3
+			     : (mask & 0xff00) ? 2 : 1);
+		  bitmap_clear_range (livenow, 4 * rn + start, end - start);
+		}
+	      else
+		gcc_assert (MEM_P (x) || x == pc_rtx);
+	      iter.skip_subrtxes ();
+	    }
+	}
+      /* Now, process the uses.  */
+      subrtx_var_iterator::array_type array_var;
+      FOR_EACH_SUBRTX_VAR (iter, array_var, PATTERN (insn), NONCONST)
+	{
+	  rtx x = *iter;
+	  enum rtx_code xcode = GET_CODE (x);
+
+	  if (GET_CODE (x) == SET)
+	    {
+	      const_rtx dst = SET_DEST (x);
+	      rtx src = SET_SRC (x);
+	      const_rtx y;
+	      unsigned HOST_WIDE_INT bit = 0;
+	      enum rtx_code code = GET_CODE (src);
+	      if (GET_CODE (dst) == SUBREG)
+		{
+		  bit = SUBREG_BYTE (dst).to_constant () * BITS_PER_UNIT;
+		  if (WORDS_BIG_ENDIAN)
+		    bit = (GET_MODE_BITSIZE (GET_MODE (dst)).to_constant ()
+			   - BITS_PER_WORD - bit);
+		  if (bit >= HOST_BITS_PER_WIDE_INT)
+		    bit = HOST_BITS_PER_WIDE_INT - 1;
+		  dst = SUBREG_REG (dst);
+		}
+	      else if (GET_CODE (dst) == ZERO_EXTRACT
+		       || GET_CODE (dst) == STRICT_LOW_PART)
+		dst = XEXP (dst, 0);
+	      if (REG_P (dst)
+		  && (code == PLUS || code == MINUS || code == MULT
+		      || code == ASHIFT
+		      || code == ZERO_EXTEND || code == SIGN_EXTEND
+		      || code == AND || code == IOR || code == XOR
+		      || code == REG
+		      || (code == SUBREG && REG_P (SUBREG_REG (src)))))
+		{
+		  unsigned HOST_WIDE_INT mask_array[]
+		    = { 0xff, 0xff00, 0xffff0000, -0x100000000 };
+		  HOST_WIDE_INT mask = 0;
+		  HOST_WIDE_INT rn = REGNO (x);
+		  for (int i = 0; i < 4; i++)
+		    if (bitmap_bit_p (live_tmp, 4 * rn + i))
+		      mask |= mask_array[i];
+		  mask >>= bit;
+		  if (code == SIGN_EXTEND || code == ZERO_EXTEND)
+		    {
+		      rtx inner = XEXP (src, 0);
+		      HOST_WIDE_INT mask2 = GET_MODE_MASK (GET_MODE (inner));
+
+		      /* Delete dead sign / zero extensions.  */
+		      if (modify && (mask & ~mask2) == 0)
+			validate_change
+			  (insn, &SET_SRC (x),
+			   simplify_gen_subreg
+			     (GET_MODE (src), inner, GET_MODE (inner), 0),
+			   false);
+
+		      mask &= mask2;
+		      src = XEXP (src, 0);
+		      code = GET_CODE (src);
+		    }
+		  if (code == PLUS || code == MINUS || code == MULT
+		      || code == ASHIFT)
+		    mask = (2ULL << floor_log2 (mask)) - 1;
+		  if (BINARY_P (src))
+		    y = XEXP (src, 0);
+		  else
+		    y = src;
+		  for (;;)
+		    {
+		      if (GET_CODE (y) == SUBREG)
+			{
+			  bit = SUBREG_BYTE (y).to_constant () * BITS_PER_UNIT;
+			  if (WORDS_BIG_ENDIAN)
+			    bit = (GET_MODE_BITSIZE
+				    (GET_MODE (y)).to_constant ()
+				     - BITS_PER_WORD - bit);
+			  if (mask)
+			    {
+			      mask <<= bit;
+			      if (!mask)
+				mask = -100000000ULL;
+			    }
+			  y = SUBREG_REG (y);
+			}
+		      if (!REG_P (y))
+			break;
+		      rn = REGNO (y);
+		      if (mask & 0xff)
+			bitmap_set_bit (livenow, rn);
+		      if (mask & 0xff00)
+			bitmap_set_bit (livenow, rn+1);
+		      if (mask & 0xffff0000)
+			bitmap_set_bit (livenow, rn+2);
+		      if (mask & -0x100000000ULL)
+			bitmap_set_bit (livenow, rn+3);
+		      if (!BINARY_P (src))
+			break;
+		      y = XEXP (src, 1), src = pc_rtx;
+		    }
+		  if (REG_P (y))
+		    iter.skip_subrtxes ();
+		}
+	      else if (REG_P (dst))
+		iter.substitute (src);
+	    }
+	  else if (xcode == SUBREG
+		   && GET_MODE_BITSIZE (GET_MODE  (x)).to_constant () <= 32
+		   && SUBREG_BYTE (x).to_constant () == 0
+		   && REG_P (SUBREG_REG (x)))
+	    {
+	      HOST_WIDE_INT size
+		= GET_MODE_BITSIZE (GET_MODE  (x)).to_constant ();
+	      HOST_WIDE_INT rn = 4 * REGNO (SUBREG_REG (x));
+	      bitmap_set_bit (livenow, rn);
+	      if (size > 8)
+		bitmap_set_bit (livenow, rn+1);
+	      if (size > 16)
+		bitmap_set_bit (livenow, rn+2);
+	      iter.skip_subrtxes ();
+	    }
+	  else if (REG_P (x))
+	    bitmap_set_range (livenow, REGNO (x) * 4, 4);
+	}
+      BITMAP_FREE (live_tmp);
+    }
+  return livenow;
+}
+
 void
 ext_dce (void)
 {
   basic_block bb, *worklist, *qin, *qout, *qend;
   unsigned int qlen;
-  rtx_insn *insn;
   vec<bitmap_head> livein;
   bitmap livenow;
 
@@ -49,227 +235,75 @@ ext_dce (void)
 
   livenow = BITMAP_ALLOC (NULL);
 
-  qin = worklist
+  worklist
     = XNEWVEC (basic_block, n_basic_blocks_for_fn (cfun) - NUM_FIXED_BLOCKS);
-  /* Put every block on the worklist.  */
-  auto_vec<int, 20> postorder;
-  inverted_post_order_compute (&postorder);
-  for (unsigned int i = 0; i < postorder.length (); ++i)
+
+  int modify = 0;
+
+  do
     {
-      bb = BASIC_BLOCK_FOR_FN (cfun, postorder[i]);
-      if (bb == EXIT_BLOCK_PTR_FOR_FN (cfun)
-	  || bb == ENTRY_BLOCK_PTR_FOR_FN (cfun))
-	continue;
-      *qin++ = bb;
-      bb->aux = bb;
-    }
+      qin = qout = worklist;
 
-  qin = worklist;
-  qend = &worklist[n_basic_blocks_for_fn (cfun) - NUM_FIXED_BLOCKS];
-  qlen = n_basic_blocks_for_fn (cfun) - NUM_FIXED_BLOCKS;
-
-  /* Iterate until the worklist is empty.  */
-  while (qlen)
-    {
-      /* Take the first entry off the worklist.  */
-      bb = *qout++;
-      qlen--;
-
-      if (qout >= qend)
-	qout = worklist;
-
-      bitmap_clear (livenow);
-      /* Make everything live that's live in the successors.  */
-      edge_iterator ei;
-      edge e;
-
-      FOR_EACH_EDGE (e, ei, bb->succs)
-	bitmap_ior_into (livenow, &livein[e->dest->index]);
-
-      FOR_BB_INSNS_REVERSE (bb, insn)
+      /* Put every block on the worklist.  */
+      auto_vec<int, 20> postorder;
+      inverted_post_order_compute (&postorder);
+      for (unsigned int i = 0; i < postorder.length (); ++i)
 	{
-	  subrtx_iterator::array_type array;
-
-	  if (!INSN_P (insn))
+	  bb = BASIC_BLOCK_FOR_FN (cfun, postorder[i]);
+	  if (bb == EXIT_BLOCK_PTR_FOR_FN (cfun)
+	      || bb == ENTRY_BLOCK_PTR_FOR_FN (cfun))
 	    continue;
-
-	  bitmap live_tmp = BITMAP_ALLOC (NULL);
-	  /* First, process the sets.  */
-	  FOR_EACH_SUBRTX (iter, array, PATTERN (insn), NONCONST)
-	    {
-	      const_rtx x = *iter;
-	      if (GET_CODE (x) == SET || GET_CODE (x) == CLOBBER)
-		{
-		  unsigned bit = 0;
-		  unsigned HOST_WIDE_INT mask = GET_MODE_MASK (GET_MODE (x));
-		  x = SET_DEST (x);
-		  if (GET_CODE (x) == SUBREG)
-		    {
-		      bit = SUBREG_BYTE (x).to_constant () * BITS_PER_UNIT;
-		      if (WORDS_BIG_ENDIAN)
-			bit = (GET_MODE_BITSIZE (GET_MODE (x)).to_constant ()
-			       - BITS_PER_WORD - bit);
-		      mask = GET_MODE_MASK (GET_MODE (SUBREG_REG (x))) << bit;
-		      if (!mask)
-			mask = -100000000ULL;
-		      x = SUBREG_REG (x);
-		    }
-		  else if (GET_CODE (x) == STRICT_LOW_PART)
-		    {
-		      x = XEXP (x, 0);
-		    }
-		  if (REG_P (x))
-		    {
-		      HOST_WIDE_INT rn = REGNO (x);
-		      for (HOST_WIDE_INT i = 4 * rn; i < 4 * rn + 4; i++)
-			if (bitmap_bit_p (livenow, i))
-			  bitmap_set_bit (live_tmp, i);
-		      int start = (bit == 0 ? 0 : bit == 8 ? 1
-				   : bit == 16 ? 2 : 3);
-		      int end = ((mask & ~0xffffffffULL) ? 4
-				 : (mask & 0xffff0000) ? 3
-				 : (mask & 0xff00) ? 2 : 1);
-		      bitmap_clear_range (livenow, 4 * rn + start, end - start);
-		    }
-		  else
-		    gcc_assert (MEM_P (x));
-		  iter.skip_subrtxes ();
-		}
-	    }
-	  /* Now, process the uses.  */
-	  FOR_EACH_SUBRTX (iter, array, PATTERN (insn), NONCONST)
-	    {
-	      const_rtx x = *iter;
-	      enum rtx_code xcode = GET_CODE (x);
-
-	      if (GET_CODE (x) == SET)
-		{
-		  const_rtx dst = SET_DEST (x);
-		  const_rtx src = SET_SRC (x);
-		  unsigned HOST_WIDE_INT bit = 0;
-		  enum rtx_code code = GET_CODE (src);
-		  if (GET_CODE (dst) == SUBREG)
-		    {
-		      bit = SUBREG_BYTE (dst).to_constant () * BITS_PER_UNIT;
-		      if (WORDS_BIG_ENDIAN)
-			bit = (GET_MODE_BITSIZE (GET_MODE (dst)).to_constant ()
-			       - BITS_PER_WORD - bit);
-		      if (bit >= HOST_BITS_PER_WIDE_INT)
-			bit = HOST_BITS_PER_WIDE_INT - 1;
-		      dst = SUBREG_REG (dst);
-		    }
-		  else if (GET_CODE (dst) == ZERO_EXTRACT
-		      || GET_CODE (dst) == STRICT_LOW_PART)
-		    dst = XEXP (dst, 0);
-		  if (REG_P (dst)
-		      && (code == PLUS || code == MINUS || code == MULT
-			  || code == ASHIFT
-			  || code == ZERO_EXTEND || code == SIGN_EXTEND
-			  || code == AND || code == IOR || code == XOR
-			  || code == REG
-			  || (code == SUBREG && REG_P (SUBREG_REG (src)))))
-		    {
-		      unsigned HOST_WIDE_INT mask_array[]
-			= { 0xff, 0xff00, 0xffff0000, -0x100000000 };
-		      HOST_WIDE_INT mask = 0;
-		      HOST_WIDE_INT rn = REGNO (x);
-		      for (int i = 0; i < 4; i++)
-			if (bitmap_bit_p (live_tmp, 4 * rn + i))
-			  mask |= mask_array[i];
-		      mask >>= bit;
-		      if (code == SIGN_EXTEND || code == ZERO_EXTEND)
-			{
-			  mask &= GET_MODE_MASK (GET_MODE (XEXP (src, 0)));
-			  src = XEXP (src, 0);
-			  code = GET_CODE (src);
-			}
-		      if (code == PLUS || code == MINUS || code == MULT
-			  || code == ASHIFT)
-			mask = (2ULL << floor_log2 (mask)) - 1;
-		      if (BINARY_P (src))
-			x = XEXP (src, 0);
-		      else
-			x = src;
-		      for (;;)
-			{
-			  if (GET_CODE (x) == SUBREG)
-			    {
-			      bit = (SUBREG_BYTE (x).to_constant ()
-				     * BITS_PER_UNIT);
-			      if (WORDS_BIG_ENDIAN)
-				bit = (GET_MODE_BITSIZE
-					(GET_MODE (x)).to_constant ()
-					 - BITS_PER_WORD - bit);
-			      if (mask)
-				{
-				  mask <<= bit;
-				  if (!mask)
-				    mask = -100000000ULL;
-				}
-			      x = SUBREG_REG (x);
-			    }
-			  if (!REG_P (x))
-			    break;
-			  rn = REGNO (x);
-			  if (mask & 0xff)
-			    bitmap_set_bit (livenow, rn);
-			  if (mask & 0xff00)
-			    bitmap_set_bit (livenow, rn+1);
-			  if (mask & 0xffff0000)
-			    bitmap_set_bit (livenow, rn+2);
-			  if (mask & -0x100000000ULL)
-			    bitmap_set_bit (livenow, rn+3);
-			  if (!BINARY_P (src))
-			    break;
-			  x = XEXP (src, 1);
-			}
-		      if (REG_P (x))
-			iter.skip_subrtxes ();
-		    }
-		  else if (REG_P (dst))
-		    iter.substitute (src);
-		}
-	      else if (xcode == SUBREG
-		       && GET_MODE_BITSIZE (GET_MODE  (x)).to_constant () <= 32
-		       && SUBREG_BYTE (x).to_constant () == 0
-		       && REG_P (SUBREG_REG (x)))
-		{
-		  HOST_WIDE_INT size
-		    = GET_MODE_BITSIZE (GET_MODE  (x)).to_constant ();
-		  HOST_WIDE_INT rn = 4 * REGNO (SUBREG_REG (x));
-		  bitmap_set_bit (livenow, rn);
-		  if (size > 8)
-		    bitmap_set_bit (livenow, rn+1);
-		  if (size > 16)
-		    bitmap_set_bit (livenow, rn+2);
-		  iter.skip_subrtxes ();
-		}
-	      else if (REG_P (x))
-		bitmap_set_range (livenow, REGNO (x) * 4, 4);
-	    }
-	  BITMAP_FREE (live_tmp);
+	  *qin++ = bb;
+	  bb->aux = bb;
 	}
 
-      if (!bitmap_equal_p (&livein[bb->index], livenow))
-	{
-	  bitmap_copy (&livein[bb->index], livenow);
+      qin = worklist;
+      qend = &worklist[n_basic_blocks_for_fn (cfun) - NUM_FIXED_BLOCKS];
+      qlen = n_basic_blocks_for_fn (cfun) - NUM_FIXED_BLOCKS;
 
+      /* Iterate until the worklist is empty.  */
+      while (qlen)
+	{
+	  /* Take the first entry off the worklist.  */
+	  bb = *qout++;
+	  qlen--;
+
+	  if (qout >= qend)
+	    qout = worklist;
+
+	  /* Clear the aux field of this block so that it can be added to
+	     the worklist again if necessary.  */
+	  bb->aux = NULL;
+
+	  bitmap_clear (livenow);
+	  /* Make everything live that's live in the successors.  */
 	  edge_iterator ei;
 	  edge e;
 
-	  FOR_EACH_EDGE (e, ei, bb->preds)
-	    if (!e->src->aux && e->src != ENTRY_BLOCK_PTR_FOR_FN (cfun))
-	      {
-		*qin++ = e->src;
-		e->src->aux = e;
-		qlen++;
-		if (qin >= qend)
-		  qin = worklist;
-	      }
-	}
-    }
+	  FOR_EACH_EDGE (e, ei, bb->succs)
+	    bitmap_ior_into (livenow, &livein[e->dest->index]);
 
-  /* FIXME: Delete dead sign / zero extensions.  */
+	  livenow = ext_dce_process_bb (bb, livenow, modify > 0);
+
+	  if (!bitmap_equal_p (&livein[bb->index], livenow))
+	    {
+	      bitmap_copy (&livein[bb->index], livenow);
+
+	      edge_iterator ei;
+	      edge e;
+
+	      FOR_EACH_EDGE (e, ei, bb->preds)
+		if (!e->src->aux && e->src != ENTRY_BLOCK_PTR_FOR_FN (cfun))
+		  {
+		    *qin++ = e->src;
+		    e->src->aux = e;
+		    qlen++;
+		    if (qin >= qend)
+		      qin = worklist;
+		  }
+	    }
+	}
+    } while (!modify++);
 
   /* Clean up.  */
   BITMAP_FREE (livenow);
@@ -279,4 +313,44 @@ ext_dce (void)
   livein.release ();
   clear_aux_for_blocks ();
   free (worklist);
+}
+
+namespace {
+
+const pass_data pass_data_ext_dce =
+{
+  RTL_PASS, /* type */
+  "ext_dce", /* name */
+  OPTGROUP_NONE, /* optinfo_flags */
+  TV_NONE, /* tv_id */
+  PROP_cfglayout, /* properties_required */
+  0, /* properties_provided */
+  0, /* properties_destroyed */
+  0, /* todo_flags_start */
+  TODO_df_finish, /* todo_flags_finish */
+};
+
+class pass_ext_dce : public rtl_opt_pass
+{
+public:
+  pass_ext_dce (gcc::context *ctxt)
+    : rtl_opt_pass (pass_data_ext_dce, ctxt)
+  {}
+
+  /* opt_pass methods: */
+  virtual bool gate (function *) { return ( flag_ext_dce); }
+  virtual unsigned int execute (function *)
+    {
+      ext_dce ();
+      return 0;
+    }
+
+}; // class pass_combine
+
+} // anon namespace
+
+rtl_opt_pass *
+make_pass_ext_dce (gcc::context *ctxt)
+{
+  return new pass_ext_dce (ctxt);
 }
