c45077f1700d0f72071e34911eb280433745f421
Joern Rennecke <joern.rennecke@embecosm.com>
2023-05-11 16:40:23 +0100

    Fix/ addendum to last change:
    [amylaar@damia testsuite]$ cat edcmds
    g/tvms\(eq\|ne\)\\\.vx".*rv32/s/66/88/
    w
    q

--- gcc/testsuite/gcc.target/riscv/rvv/vectorization/condition/cond_vi_2_same_type_3.c
+++ gcc/testsuite/gcc.target/riscv/rvv/vectorization/condition/cond_vi_2_same_type_3.c
@@ -44,7 +44,7 @@
 
 TEST_ALL (TEST_LOOP)
 
-/* { dg-final { scan-assembler-times "\tvmseq\.vx" 66 {target { rv32 }} } } */
+/* { dg-final { scan-assembler-times "\tvmseq\.vx" 88 {target { rv32 }} } } */
 /* { dg-final { scan-assembler-times "\tvmerge\.vxm" 88 {target { rv32 }} } } */
 /* { dg-final { scan-assembler-times "\tvmerge\.vvm" 22 {target { rv32 }} } } */
 /* { dg-final { scan-assembler-times "\tvfmerge\.vfm" 33 {target { rv32 }} } } */
--- gcc/testsuite/gcc.target/riscv/rvv/vectorization/condition/cond_vi_3_same_type_9.c
+++ gcc/testsuite/gcc.target/riscv/rvv/vectorization/condition/cond_vi_3_same_type_9.c
@@ -44,7 +44,7 @@
 
 TEST_ALL (TEST_LOOP)
 
-/* { dg-final { scan-assembler-times "\tvmsne\.vx" 66 {target { rv32 }} } } */
+/* { dg-final { scan-assembler-times "\tvmsne\.vx" 88 {target { rv32 }} } } */
 /* { dg-final { scan-assembler-times "\tvmerge\.vxm" 88 {target { rv32 }} } } */
 /* { dg-final { scan-assembler-times "\tvmerge\.vvm" 22 {target { rv32 }} } } */
 /* { dg-final { scan-assembler-times "\tvfmerge\.vfm" 33 {target { rv32 }} } } */
